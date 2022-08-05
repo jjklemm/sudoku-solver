1d7745be2ab28649b0cde0728855b103aa8f290e
Jared Klemm <jarkle00@outlook.com>
2022-08-04 20:48:10 -0600

    Working on GUI (WIP)

--- com/jjklemm/sudokuSolver/sudokuSolver.java
+++ com/jjklemm/sudokuSolver/sudokuSolver.java
@@ -8,20 +8,41 @@
 
 package com.jjklemm.sudokuSolver;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import java.util.Map;
+import java.awt.Point;
+import java.util.HashMap;
 
 public class sudokuSolver extends JFrame {
-    //JButton newBoardButton = new JButton("New Board");
+    // numeric constants
+    private final int GRID_SIZE = 9;
+    // UI elements
+    JButton clearBoardButton = new JButton("Clear Board");
     JButton solveBoardButton = new JButton("Solve Puzzle");
+    JTextField[][] board = new JTextField[GRID_SIZE][GRID_SIZE];
 
     public sudokuSolver() {
         super("Sudoku Solver");
+        // initialize board
+        for (int i=0; i<GRID_SIZE; i++){
+            for (int j=0; j<GRID_SIZE; j++){
+                board[i][j] = new JTextField(1);
+            }
+        }
         setSize(1200, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel solvePane = new JPanel();
-        solvePane.add(solveBoardButton);
-        add(solvePane);
+        JPanel controlPane = new JPanel();
+        controlPane.add(solveBoardButton);
+        controlPane.add(clearBoardButton);
+        controlPane.add(board[0][0]);
+        controlPane.add(board[0][1]);
+        add(controlPane);
         setVisible(true);
+
     }
 
     private static void setLookAndFeel() {
